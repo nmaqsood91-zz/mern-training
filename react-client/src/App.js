3878cedbf0aaf950501ee199859d9d35904a502a
Noman Maqsood <nmaqsood91@gmail.com>
2020-01-04 12:14:20 +0500

    react files

---
+++ react-client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './Nav'
+import './index.css';
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      Hello
+    </div>
+  );
+}
+
+export default App;
