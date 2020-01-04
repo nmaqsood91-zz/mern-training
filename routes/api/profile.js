bff8b53a3f4f5d5824e5723161b065abb1a124c0
Noman Maqsood <nmaqsood91@gmail.com>
2020-01-04 12:06:27 +0500

    added the github endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const verifyToken = require("../../middleare/verifyToken")
 const Profile = require("../../models/Profile")
+const request = require("request")
+
 router.get("/", verifyToken, async (req, res) =>  {
     try {
         const _user = await Profile.findOne({
@@ -27,6 +29,27 @@ router.post("/", verifyToken, async function (req, res) {
         return res.status(400).json(err)
     }
 
+})
+
+router.get("/github/:username", async (req,res) => {
+    try {
+        const optiosn = {
+            uri: `https://api.github.com/users/${req.params.username}/repo?per_page=5&sort=created:asc&
+            cleint_id=${github_client}&client_secret=${github_secret}`,
+            method: 'GET',
+            headers: {
+                'user-agent':  'node.js'
+            }
+        }
+        request.get(optiosn, (err, resp, body) => {
+            if(err)
+                console.log(err)
+            res.json(body)
+        })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json(err)
+    }
 })
 
 module.exports = router
