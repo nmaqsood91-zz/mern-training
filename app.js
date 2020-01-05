2c736b29dccfea91a0828488286e33573b8081a6
Noman Maqsood <nmaqsood91@gmail.com>
2020-01-05 12:49:16 +0500

    Added procfile

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const {connect} = require("./models/index")
 connect()
 /**
@@ -8,7 +9,7 @@ connect()
 const user = require("./routes/api/users")
 const profile = require("./routes/api/profile")
 const auth = require("./routes/api/auth")
-
+app.use(cors())
  /** express middleware to parse JSON body */
  app.use(express.json({urlencoded: false}))
 
@@ -19,6 +20,6 @@ app.use("/api/v1/users", user)
 app.use("/api/v1/auth", auth)
 app.use("/api/v1/profile", profile)
 app.use("/api/v1/posts", require("./routes/api/post"))
-app.listen(3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(' Server is up and running ')
 })
